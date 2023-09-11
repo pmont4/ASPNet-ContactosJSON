6a98db129d06a7b38fa3e89b3de06a35da8163fa
pmont4 <pauloamont2005@outlook.com>
2023-09-11 16:49:58 -0600

    .

--- ASPWeb-Demo2/Program.cs
+++ ASPWeb-Demo2/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Contacto}/{action=Inicio}/{id?}");
+    pattern: "{controller=LogIn}/{action=Login}/{id?}");
 
 app.Run();
