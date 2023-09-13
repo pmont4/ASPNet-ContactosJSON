114bbce6fbfe798a1173d9292877780ab9142fea
pmont4 <pauloamont2005@outlook.com>
2023-09-13 12:07:28 -0600

    .

--- ASPWeb-Demo2/Models/Sesion.cs
+++ ASPWeb-Demo2/Models/Sesion.cs
@@ -1,9 +1,39 @@
-﻿namespace ASPWeb_Demo2.Models
+﻿using Newtonsoft.Json;
+
+namespace ASPWeb_Demo2.Models
 {
+
+    [Serializable]
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     public class Sesion
     {
-        private int _id;
 
-        public int Id { get { return _id; } set { _id = value; } }
+        public Sesion() { }
+
+        [JsonProperty("idSesion")]
+        private int idSesion;
+
+        [JsonProperty("fecha")]
+        private string fecha;
+
+        [JsonProperty("registros")]
+        private List<string> registros;
+
+        public int getIdSesion() => this.idSesion;
+
+        public void setIdSesion(int id) => this.idSesion = id;
+
+        public string getFecha() => this.fecha;
+
+        public List<string> getRegistros() => this.registros;
+
+        public void setRegistros(List<string> registros) => this.registros = registros;
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
     }
+
 }
