975d5499b8ad23f6dbc8325f54998d8b93990d7a
pmont4 <pauloamont2005@outlook.com>
2023-09-21 14:50:49 -0600

    .

---
+++ ASPWeb-Demo2/Models/Registro.cs
@@ -0,0 +1,43 @@
+﻿namespace ASPWeb_Demo2.Models
+{
+
+    [Serializable]
+    public class Registro
+    {
+
+        private int idRegistro;
+        private string texto;
+        private string fecha;
+
+        public Registro() { }
+
+        public Registro(int idRegistro, string texto, string fecha)
+        {
+            this.idRegistro = idRegistro;
+            this.texto = texto;
+            this.fecha = fecha;
+        }
+
+        public int getIdRegistro() => this.idRegistro;
+
+        public void setIdRegistro(int id)
+        {
+            this.idRegistro = id;
+        }
+
+        public string getTexto() => this.texto;
+
+        public void setTexto(string texto) 
+        { 
+            this.texto = texto; 
+        }
+
+        public string getFecha() => this.fecha;
+
+        public void setFecha(string fecha)
+        {
+            this.fecha = fecha;
+        }
+
+    }
+}
